bd809794360af73b45dc5aae04484b04834a6453
Luis Ricardo <luisricardomancilla@hotmail.com>
2019-01-29 15:05:22 -0500

    Creating the ProductsListComponent component.

---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent {
+
+  @Input() productList: Product[];
+  @Output() onProductSelected: EventEmitter<Product>;
+
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product: Product) {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if(!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
+
+}
