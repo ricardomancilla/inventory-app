2b7693aa734eafbac611f83803f0a637f6ff011a
Luis Ricardo <luisricardomancilla@hotmail.com>
2019-01-29 12:56:18 -0500

    Fix productList issue.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
